ab557b56d56c9f975657dcd82febd5aa1e1e990b
Ken Thooptong <ken.thooptong@gmail.com>
2019-02-09 18:41:57 +0000

    where is my response

--- https_example.js
+++ https_example.js
@@ -6,12 +6,12 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
-
-console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
